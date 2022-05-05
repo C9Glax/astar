6eae4df1f3917ed30633f7fc375a530b8fec57b4
C9Glax <gfreak70@gmail.com>
2022-05-06 00:49:16 +0200

    test

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,3 +1,4 @@
-﻿Logging.Logger logger = new Logging.Logger(Logging.LogType.Console, Logging.loglevel.DEBUG);
+﻿//test
+Logging.Logger logger = new Logging.Logger(Logging.LogType.Console, Logging.loglevel.DEBUG);
 Dictionary<UInt64, Graph.Node> nodes = OpenStreetMap_Importer.Importer.Import(logger);
 new astar.Astar(nodes, logger);
