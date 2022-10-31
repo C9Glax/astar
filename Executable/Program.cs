bd06937b559b290389a3b0980d138e877a77b493
C9Glax <13404778+C9Glax@users.noreply.github.com>
2022-10-31 23:10:33 +0100

    Readkey

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,4 +1,5 @@
 ﻿Logging.Logger logger = new (Logging.LogType.CONSOLE, Logging.LogLevel.DEBUG);
-Dictionary<UInt64, Graph.Node> nodes = OpenStreetMap_Importer.Importer.Import(@"", logger);
+Dictionary<UInt64, Graph.Node> nodes = OpenStreetMap_Importer.Importer.Import(@"C:\Users\glax\Downloads\oberbayern-latest.osm", logger);
 logger.level = Logging.LogLevel.VERBOSE;
 astar.Astar astar = new(nodes, logger);
+Console.ReadKey();
